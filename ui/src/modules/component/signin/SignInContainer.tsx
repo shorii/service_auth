743f381eca37293f7cda8cff5d9000f7a3305024
s-horii <s2010310horii@gmail.com>
2022-06-14 11:10:19 +0900

    Add ui base

---
+++ ui/src/modules/component/signin/SignInContainer.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export interface SignInContainerProps {}
+
+export const SignInContainer: React.FC<SignInContainerProps> = (props) => {
+    return <>sign in container</>;
+};
---
+++ ui/src/modules/component/signup/SignUpContainer.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export interface SignUpContainerProps {}
+
+export const SignUpContainer: React.FC<SignUpContainerProps> = (props) => {
+    return <>sign up container</>;
+};
